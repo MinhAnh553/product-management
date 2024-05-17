2a8471337c8ce8a0f5053abb02fcec67e6162e4c
MinhAnhIT <anhhoclaptrinh@gmail.com>
2024-05-17 17:12:04 +0700

    Admin: Xem trước ảnh upload

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -141,3 +141,19 @@ if (showAlert) {
         showAlert.classList.add('alert-hidden');
     });
 }
+
+// Preview image
+const uploadImage = document.querySelector('[upload-image]');
+if (uploadImage) {
+    const uploadImageInput = uploadImage.querySelector('[upload-image-input]');
+    const uploadImagePreview = uploadImage.querySelector(
+        '[upload-image-preview]'
+    );
+
+    uploadImageInput.addEventListener('change', (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
