b268aa413370bddc90f43a08956d6c157a292a76
MinhAnh553 <huynhminhanh553@gmail.com>
2024-06-08 17:02:12 +0700

    Admin: Fix STT

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,9 +1,12 @@
 const createTree = (arr, parentId = '') => {
+    let count = 0;
     const tree = [];
     arr.forEach((item) => {
+        count++;
         if (item.parent_id == parentId) {
             const newItem = item;
             const children = createTree(arr, item.id);
+            newItem.index = count;
             if (children.length > 0) {
                 newItem.children = children;
             }
