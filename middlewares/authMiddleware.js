fe2e8625b8a2565ff3875c0386e6f527d3b1ab8d
MinhAnh553 <huynhminhanh553@gmail.com>
2024-06-30 15:15:53 +0700

    Admin: Thêm authorization

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,22 @@
 const accountModel = require('../models/accountModel');
+const roleModel = require('../models/roleModel');
 const system = require('../config/system');
 
 module.exports.requireAuth = async (req, res, next) => {
     const token = req.cookies.token;
     if (token) {
-        const user = accountModel.findOne({
-            token: token,
-            deleted: false,
-        });
+        const user = await accountModel
+            .findOne({
+                token: token,
+                deleted: false,
+            })
+            .select('-password');
         if (user) {
+            const role = await roleModel.findOne({
+                _id: user.role_id,
+            });
+
+            res.locals.role = role;
             next();
         } else {
             res.redirect(`${system.prefixAdmin}/auth/login`);
