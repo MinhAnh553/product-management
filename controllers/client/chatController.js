a5aabbd27314594aa772e8e90fd897757b4f49ca
MinhAnh553 <huynhminhanh553@gmail.com>
2024-07-18 15:41:24 +0700

    Tính năng chat cơ bản

--- controllers/client/chatController.js
+++ controllers/client/chatController.js
@@ -1,10 +1,37 @@
+const chatModel = require('../../models/chatModel');
+const userModel = require('../../models/userModel');
+
 // [GET] /chat
-module.exports.index = (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+module.exports.index = async (req, res) => {
+    _io.once('connection', (socket) => {
+        socket.on('CLIENT_SEND_MESSAGE', async (content) => {
+            const chat = new chatModel({
+                user_id: res.locals.user.id,
+                content: content,
+            });
+            await chat.save();
+        });
     });
+
+    const chats = await chatModel.find({
+        deleted: false,
+    });
+
+    if (chats) {
+        for (const chat of chats) {
+            const user = await userModel
+                .findOne({
+                    _id: chat.user_id,
+                    deleted: false,
+                })
+                .select('fullName');
+
+            chat.infoUser = user;
+        }
+    }
 
     res.render('client/pages/chat/index.pug', {
         pageTitle: 'Chat',
+        chats: chats,
     });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-const socket = io();
+// CLIENT_SEND_MESSAGE
+const formMessage = document.querySelector('.chat .inner-form');
+if (formMessage) {
+    formMessage.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = e.target.elements.content.value;
+        socket.emit('CLIENT_SEND_MESSAGE', message);
+        e.target.elements.content.value = '';
+    });
+}
